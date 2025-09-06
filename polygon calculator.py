b6d4d297e8ecf8030f8495503b7bbe03977fc2d7
Fola-b3 <holartobad@gmail.com>
2025-09-06 15:31:59 -0400

    Update polygon calculator.py
    Listed "DATA VIS EDIT" ON FILE

--- polygon calculator.py
+++ polygon calculator.py
@@ -2,8 +2,8 @@
 ##################################
 #Olamide Afolabi
 #Polygoncalculator.py
-#In this project, you will create a method called calcPolygon() that will accept two numbers as a parameters which are the number of sides of the polygon and the length of one of the sides. The method will return a number that is the area of the polygon. The method will calculate the area in square feet of the polygon using the formula:
-#04/28/2020
+#DATA VIS EDIT FOR HW 1
+#09/06/2025
 #################################
 
 
